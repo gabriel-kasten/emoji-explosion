a79215d952248cf76068a9e997b60c537188c1e2
Gabriel Kasten <gabrieldegkasten@gmail.com>
2023-04-14 10:33:18 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,39 +1,39 @@
-const containerSlot = document.querySelector('.slot');
-const btnConfettis = document.querySelector('.fiesta');
-const emojis = ["👾", "👽", "🐱‍🏍", "🧠", "🐱‍👤", "🤓", "🥳", "🤯", "🤪", "😵"];
+const containerSlot = document.querySelector(".slot");
+const btnConfettis = document.querySelector(".btn-confettis");
+const emojis = ["🍰", "🍣", "🍑", "🍓"];
 
-btnConfettis.addEventListener('click', fiesta);
+btnConfettis.addEventListener("click", fiesta);
 
 function fiesta() {
+  
+  if(isTweening()) return;
 
-    if (isTweening()) return;
+  for (let i = 0; i < 50; i++) {
+    const confetti = document.createElement("div");
+    confetti.innerText = emojis[Math.floor(Math.random() * emojis.length)];
+    containerSlot.appendChild(confetti);
+  }
 
-    for (let i = 0; i < 50; i++) {
-        const confetti = document.createElement('div');
-        confetti.innerText = emojis[Math.floor(Math.random() * emojis.length)];
-        containerSlot.appendChild(confetti);
-    }
-
-    animateConfettis();
+  animateConfettis();
 }
 
 function animateConfettis() {
-    const TLCONF = gsap.timeline();
+  
+  const TLCONF = gsap.timeline();
 
-    TLCONF
-        .to('.slot div', {
-            y: "random(-100,100)",
-            x: "random(-100,100)",
-            z: "random(0,1000)",
-            rotation: "random(-90, 90)",
-            duration: 1
-        })
-        .to('.slot div', { autoAlpha: 0, duration: 0.3 }, '-=0.2')
-        .add(() => {
-            containerSlot.innerHTML = "";
-        });
+  TLCONF.to(".slot div", {
+    y: "random(-100,100)",
+    x: "random(-100,100)",
+    z: "random(0,1000)",
+    rotation: "random(-90,90)",
+    duration: 1,
+  })
+    .to(".slot div", { autoAlpha: 0, duration: 0.3 }, "-=0.2")
+    .add(() => {
+      containerSlot.innerHTML = "";
+    });
 }
 
-function isTweening() {
-    return gsap.isTweening('.slot div');
+function isTweening(){
+  return gsap.isTweening('.slot div');
 }
